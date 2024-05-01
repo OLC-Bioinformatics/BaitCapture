debc63a2dd16ab46d9295856a3fc7a3581bd1999
pcrxn <pcrxn@protonmail.com>
2024-05-01 14:29:06 -0400

    Add check to ensure `--pident_threshold` can't be used without `--aligner kma`

--- lib/WorkflowBaitcapture.groovy
+++ lib/WorkflowBaitcapture.groovy
@@ -16,6 +16,8 @@ class WorkflowBaitcapture {
             Nextflow.error "Missing parameter: FASTA file of gene targets not specified with e.g. '--targets targets.fa' or via a detectable config file."
         }
 
+        if (params.aligner != 'kma' && params.pident_threshold)
+            Nextflow.error "Incompatible parameters: `--pident_threshold` is only available when using KMA as the aligner (`--aligner kma`)."
     }
 
     //
